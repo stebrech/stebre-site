2adf0d1c2552831405c228a2796505b73c664811
Stefan Brechbühl <mail@stebre.ch>
2025-03-13 22:03:42 +0100

    Fix paths

--- src/_config/passthroughCopy.js
+++ src/_config/passthroughCopy.js
@@ -1,14 +1,14 @@
 export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({
-		"./assets/img/copy": "/assets/img",
-		"./assets/movies": "/assets/movies",
-		"./assets/fonts": "/assets/fonts",
-		"./assets/styles": "/assets/styles",
-		"./assets/js": "/assets/js",
+		"./src/assets/img/copy": "/assets/img",
+		"./src/assets/movies": "/assets/movies",
+		"./src/assets/fonts": "/assets/fonts",
+		"./src/assets/styles": "/assets/styles",
+		"./src/assets/js": "/assets/js",
 		"./node_modules/animate.css/animate.min.css": "/assets/styles/animate.min.css",
 		"./node_modules/body-scroll-lock-upgrade/lib": "/assets/js/body-scroll-lock-upgrade",
-		"./assets/favicons": "/",
-		"./assets/robots.txt": "/robtos.txt",
+		"./src/assets/favicons": "/",
+		"./src/assets/robots.txt": "/robtos.txt",
 		"./_copy": "/",
 		"./_redirects": "/_redirects",
 	});
