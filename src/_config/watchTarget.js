47ae5775e05c4ba2b5a8a90778fcf9ee62b26b95
Stefan Brechbühl <mail@stebre.ch>
2026-02-22 11:12:47 +0100

    Update watchTarget

--- src/_config/watchTarget.js
+++ src/_config/watchTarget.js
@@ -1,8 +1,7 @@
 export default function (eleventyConfig) {
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
-	eleventyConfig.addWatchTarget("./_templates");
-	eleventyConfig.addWatchTarget("./assets");
-	eleventyConfig.addWatchTarget("./de");
-	eleventyConfig.addWatchTarget("./en");
+
+	// Watch images for the image pipeline.
+	eleventyConfig.addWatchTarget("./src/content/**/*.{svg,webp,png,jpg,jpeg,gif}");
 }
